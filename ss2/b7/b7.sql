17bff2d8d7e8c4b1c4e92662f504c9a544f7040f
CanhVeTaiGia <lvy16012005@gmail.com>
2025-01-15 19:49:41 +0700

    add

--- ss2/b7/b7.sql
+++ ss2/b7/b7.sql
@@ -6,3 +6,23 @@ create table order_detail(
     primary key (cus_id, order_id),
     quantity int not null
 );
+
+ALTER TABLE order_detail
+ADD CONSTRAINT fk_mahd
+FOREIGN KEY (order_id) REFERENCES HoaDon(MaHD);
+
+ALTER TABLE order_detail
+ADD CONSTRAINT fk_masp
+FOREIGN KEY (product_id) REFERENCES SanPham(MaSP);
+
+SELECT 
+    HoaDon.MaHD, 
+    SanPham.MaSP, 
+    SanPham.TenSP, 
+    order_detail.quantity
+FROM 
+    HoaDon
+JOIN 
+    order_detail ON HoaDon.MaHD = order_detail.order_id
+JOIN 
+    SanPham ON order_detail.product_id = SanPham.MaSP;
